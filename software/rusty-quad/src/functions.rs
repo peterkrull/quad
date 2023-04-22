5d54b960594b2aa244e130845660ef392a954314
Peter Krull <peterkrullpeter@gmail.com>
2023-04-23 01:53:27 +0200

    Made some functions generic for all floats

--- software/rusty-quad/src/functions.rs
+++ software/rusty-quad/src/functions.rs
@@ -1,18 +1,11 @@
-use core::ops::{Add, Div, Mul, Sub};
 use num_traits::float::Float;
 
-pub fn circular<T>(num: T, min: T, max: T) -> T
-where
-    T: Float,
-{
+pub fn circular<T : Float>(num: T, min: T, max: T) -> T {
     let frac = ((num - min) / (max - min)).floor();
     num - frac * (max - min)
 }
 
-pub fn mapf<T>(num: T, in_min: T, in_max: T, out_min: T, out_max: T) -> T
-where
-    T: Sub<Output = T> + Add<Output = T> + Mul<Output = T> + Div<Output = T> + Copy,
-{
+pub fn mapf<T : Float>(num: T, in_min: T, in_max: T, out_min: T, out_max: T) -> T {
     let out_delta = out_max - out_min;
     let in_delta = in_max - in_min;
     ((num - in_min) / in_delta) * out_delta + out_min
